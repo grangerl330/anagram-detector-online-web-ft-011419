5408bb1ac141566b9e596c9ffd96157cc87e3f53
Luke Granger <grangerl330@gmail.com>
2019-01-25 16:25:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,6 @@ class Anagram
   def match(possible_anagrams)
     possible_anagrams.select {|word| word.split("").sort == @word.split("").sort}
   end 
-  # Iterate through given array of possible anangrams. .select will return an array of whatever 
+  # Iterate through given array of possible anangrams. .select will return an array of each element that makes the block true. Uses .split("").sort on each yieled word
   
 end 
