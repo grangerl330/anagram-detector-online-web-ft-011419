582825ae606d4fe6780c32cf2a9457a3525ac2ce
Luke Granger <grangerl330@gmail.com>
2019-01-25 16:28:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,6 @@ class Anagram
   def match(possible_anagrams)
     possible_anagrams.select {|word| word.split("").sort == @word.split("").sort}
   end 
-  # Iterate through given array of possible anangrams. .select will return an array of each element that makes the block true. Uses .split("").sort on each yieled word
+  # Iterates through given array of possible anangrams. .select will return an array of each element that makes the block true. The block uses .split("") to turn the yieled word into an array of letters, then .sort to organize those letters alphabetically. It compares that array to an array made from @word by the same process. If those 2 arrays are the same, that yieled word will be added to the array that is returned by .select 
   
 end 
